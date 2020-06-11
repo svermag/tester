01451654911d84a6c978dfb146492bd471f94adb
svermag <saurabh.vermag@yahoo.com>
2020-06-11 20:56:28 +0200

    Added Vignette and Documentation related to printer function

--- R/printer.R
+++ R/printer.R
@@ -1,13 +1,19 @@
-#' Title
+#' Silly Print Function
 #'
-#' @param r
-#' @param x
-#' @param y
+#' @param r What you want in second column
+#' @param x What you want in first column
+#' @param y This is still another variable
 #'
-#' @return
+#' @return A tibble
 #' @export
 #'
+#' @importFrom tibble as_data_frame
+#' @importFrom utils head adist
+#' @importFrom dplyr select
 #' @examples
+#' printer(x = rnorm(5), r = rnorm(5))
 printer = function(r, x, y) {
-    print(paste0("x = ", x))
+    x = as_data_frame(x = x, r = r)
+    print(head(x))
+    return(x)
 }
